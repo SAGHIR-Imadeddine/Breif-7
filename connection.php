9045f92a2120ac7849f7ce81a0b1fef05484467f
SoufianeAbo <abounasrsoufiane@gmail.com>
2023-12-06 16:17:18 +0100

    finish affichage

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "datawareSite";
+$dbname = "brief_7";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
