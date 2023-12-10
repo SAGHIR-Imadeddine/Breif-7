46a4a39c0817772c79b89c0da489ef5f8a906367
ATalemsi <136909197+ATalemsi@users.noreply.github.com>
2023-12-10 23:28:15 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "zakaria12";
+$password = "";
 $dbname = "brief_7";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
